89fb6f7d579ae5016bdb15839bcfd3ee9bc2b6a7
Chris Richardson <chris@chrisrichardson.net>
2020-06-16 12:21:24 -0700

    Commit for master

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-docker run $* \
+docker run ${1:--it} \
    --name mysqlterm --network=${PWD##*/}_default --rm \
    mysql:5.7.13 \
    sh -c 'exec mysql -hmysql  -uroot -prootpassword -o eventuate'
