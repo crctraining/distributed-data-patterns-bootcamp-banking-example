6c8d81eea9cb2f411445350de40e23344bf0e134
Chris Richardson <chris@chrisrichardson.net>
2021-12-19 12:41:50 -0800

    Commit for master

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,6 +2,6 @@
 
 ./gradlew assemble
 
-for service in customer-service account-service money-transfer-service customer-view-service  ; do
+for service in api-gateway-service customer-service account-service money-transfer-service customer-view-service  ; do
   (cd $service ; docker build -t $service .)
 done
