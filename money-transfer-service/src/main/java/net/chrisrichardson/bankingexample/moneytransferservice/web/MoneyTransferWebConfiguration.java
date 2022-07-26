45e1a389d241ff5162cb409c171a305694bd34ac
Chris Richardson <chris@chrisrichardson.net>
2022-07-26 10:59:46 -0700

    Commit for master

--- money-transfer-service/src/main/java/net/chrisrichardson/bankingexample/moneytransferservice/web/MoneyTransferWebConfiguration.java
+++ money-transfer-service/src/main/java/net/chrisrichardson/bankingexample/moneytransferservice/web/MoneyTransferWebConfiguration.java
@@ -1,13 +1,13 @@
 package net.chrisrichardson.bankingexample.moneytransferservice.web;
 
 import net.chrisrichardson.bankingexample.moneytransferservice.backend.MoneyTransferBackendConfiguration;
-import net.chrisrichardson.eventstore.javaexamples.banking.commonswagger.CommonSwaggerConfiguration;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({MoneyTransferBackendConfiguration.class, CommonSwaggerConfiguration.class})
+@Import({MoneyTransferBackendConfiguration.class})
 @ComponentScan
 public class MoneyTransferWebConfiguration {
 }
